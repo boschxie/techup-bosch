9bce8f01cb517622230cd9cac2ac7de3875dd3ce
boschxie <yongcongz@hotmail.com>
2024-10-29 08:01:34 +0000

    update script ver 2

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ age = 65;
 var username = "bosch";
 var password = "techup";
 
-var isMale = true;
+var isMale = false;
 var isLoggedIn = false;
 var blog_posts = ["Today was a good day", "The sky is blue", "It's raining heavily now"];
 
@@ -15,3 +15,18 @@ console.log(password);
 console.log(isMale);
 console.log(isLoggedIn);
 console.log(blog_posts);
+
+if (isMale == true){
+    console.log("You are male");
+} else {
+    console.log("You are female");
+}
+
+while (age < 70){
+    console.log("Your age is " + age);
+    age ++;
+}
+
+function sayHello(){
+    alert("Hello everyone!");
+}
